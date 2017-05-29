14312e36c24a510c53b67c0c9e64cc5ed5d990a3
Remon Lam <remon@containerstack.io>
2017-05-29 22:24:33 +0200

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-# check if the `server.xml` file has been changed since the creation of this
-# Docker image. If the file has been changed the entrypoint script will not
-# perform modifications to the configuration file.
+# Setup Catalina Opts
+: ${CATALINA_CONNECTOR_PROXYNAME:=}
+: ${CATALINA_CONNECTOR_PROXYPORT:=}
+: ${CATALINA_CONNECTOR_SCHEME:=http}
+: ${CATALINA_CONNECTOR_SECURE:=false}
 
-# Port 7990 is the Bitbucket port that's beeing described in the Dockerfile
-if [ "$(stat --format "%Y" "${BITBUCKET_INSTALL}/conf/server.xml")" -eq "0" ]; then
-  if [ -n "${X_PROXY_NAME}" ]; then
-    xmlstarlet ed --inplace --pf --ps --insert '//Connector[@port="7990"]' --type "attr" --name "proxyName" --value "${X_PROXY_NAME}" "${BITBUCKET_INSTALL}/conf/server.xml"
-  fi
-  if [ -n "${X_PROXY_PORT}" ]; then
-    xmlstarlet ed --inplace --pf --ps --insert '//Connector[@port="7990"]' --type "attr" --name "proxyPort" --value "${X_PROXY_PORT}" "${BITBUCKET_INSTALL}/conf/server.xml"
-  fi
-  if [ -n "${X_PROXY_SCHEME}" ]; then
-    xmlstarlet ed --inplace --pf --ps --insert '//Connector[@port="7990"]' --type "attr" --name "scheme" --value "${X_PROXY_SCHEME}" "${BITBUCKET_INSTALL}/conf/server.xml"
-  fi
-  if [ "${X_PROXY_SCHEME}" = "https" ]; then
-    xmlstarlet ed --inplace --pf --ps --insert '//Connector[@port="7990"]' --type "attr" --name "secure" --value "true" "${BITBUCKET_INSTALL}/conf/server.xml"
-    xmlstarlet ed --inplace --pf --ps --update '//Connector[@port="7990"]/@redirectPort' --value "${X_PROXY_PORT}" "${BITBUCKET_INSTALL}/conf/server.xml"
-  fi
-  if [ -n "${X_PATH}" ]; then
-    xmlstarlet ed --inplace --pf --ps --update '//Context/@path' --value "${X_PATH}" "${BITBUCKET_INSTALL}/conf/server.xml"
-  fi
+: ${CATALINA_OPTS:=}
+CATALINA_OPTS="${CATALINA_OPTS} -DcatalinaConnectorProxyName=${CATALINA_CONNECTOR_PROXYNAME}"
+CATALINA_OPTS="${CATALINA_OPTS} -DcatalinaConnectorProxyPort=${CATALINA_CONNECTOR_PROXYPORT}"
+CATALINA_OPTS="${CATALINA_OPTS} -DcatalinaConnectorScheme=${CATALINA_CONNECTOR_SCHEME}"
+CATALINA_OPTS="${CATALINA_OPTS} -DcatalinaConnectorSecure=${CATALINA_CONNECTOR_SECURE}"
+
+JAVA_OPTS="${JAVA_OPTS} ${CATALINA_OPTS}"
+
+# Start Bitbucket as the correct user.
+if [ "$UID" -eq 0 ]; then
+    echo "User is currently root. Will change directory ownership to ${RUN_USER}:${RUN_GROUP}, then downgrade permission to ${RUN_USER}"
+    mkdir -p "${BITBUCKET_HOME}/lib" &&
+        chmod -R 700 "${BITBUCKET_HOME}" &&
+        chown -R "${RUN_USER}:${RUN_GROUP}" "${BITBUCKET_HOME}"
+    # Now drop privileges
+    exec su -s /bin/bash "${RUN_USER}" -c "${BITBUCKET_INSTALL_DIR}/bin/start-bitbucket.sh $@"
+else
+    exec "${BITBUCKET_INSTALL_DIR}/bin/start-bitbucket.sh $@"
 fi
-
-exec "$@"
